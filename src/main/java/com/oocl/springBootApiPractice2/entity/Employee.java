c681e8ba7f64dc09844b476a8d027ef6ddf2a477
SiuKeung <siu_keung@outlook.com>
2018-07-24 17:33:35 +0800

    新建了Employee实体类，用于表示员工信息

---
+++ src/main/java/com/oocl/springBootApiPractice2/entity/Employee.java
@@ -0,0 +1,84 @@
+package com.oocl.springBootApiPractice2.entity;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+/**
+ * @author Dylan Wei
+ * @date 2018-07-24 12:41
+ */
+public class Employee {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+
+
+    public Employee(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Employee() {
+
+    }
+
+    public Employee(Integer id, String name, Integer age, String gender) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(getId(), employee.getId());
+    }
+
+}
