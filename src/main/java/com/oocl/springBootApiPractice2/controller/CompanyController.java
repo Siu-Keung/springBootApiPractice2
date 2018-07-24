611d9680526089525a76cb7beb872020351b06f7
SiuKeung <siu_keung@outlook.com>
2018-07-24 23:01:02 +0800

    完成了更新公司的controller方法

--- src/main/java/com/oocl/springBootApiPractice2/controller/CompanyController.java
+++ src/main/java/com/oocl/springBootApiPractice2/controller/CompanyController.java
@@ -5,10 +5,7 @@ import com.oocl.springBootApiPractice2.entity.Employee;
 import com.oocl.springBootApiPractice2.model.CompanyModel;
 import com.oocl.springBootApiPractice2.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,6 +43,11 @@ public class CompanyController {
     @PostMapping("/companies")
     public String addCompany(Company company){
         return this.companyService.addCompany(company) ? "succeeded" : "failed";
+    }
+
+    @PutMapping("/companies/{id}")
+    public String updateCompany(Company company){
+        return this.companyService.updateCompany(company) ? "succeeded" : "failed";
     }
 
 
