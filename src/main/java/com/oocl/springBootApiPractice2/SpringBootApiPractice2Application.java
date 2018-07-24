c73034d1d9449f68dbb79abd2db24592c4cd3f98
SiuKeung <siu_keung@outlook.com>
2018-07-24 17:26:10 +0800

    构建了项目

---
+++ src/main/java/com/oocl/springBootApiPractice2/SpringBootApiPractice2Application.java
@@ -0,0 +1,12 @@
+package com.oocl.springBootApiPractice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiPractice2Application.class, args);
+	}
+}
